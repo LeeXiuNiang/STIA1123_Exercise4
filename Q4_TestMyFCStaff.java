5dbeb4ae1ef1fb380709b3b8a27166f2c50b3420
Lee Xiu Niang <55247305+LeeXiuNiang@users.noreply.github.com>
2020-03-30 20:03:11 +0800

    Create Q4_TestMyFCStaff.java

---
+++ Q4_TestMyFCStaff.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Q4_TestMyFCStaff {
+    public static void main(String arg[]){
+        Scanner input = new Scanner(System.in);
+        String name, staffID;
+        double totalHoursWork, totalSales;
+        System.out.printf("Name: ");
+        name = input.nextLine();
+        System.out.print("Staff ID: ");
+        staffID = input.nextLine();
+        System.out.print("Working Hours: ");
+        totalHoursWork = input.nextDouble();
+        System.out.print("Total Sales: ");
+        totalSales = input.nextDouble();
+
+        MyFCStaff fcs = new MyFCStaff(name,staffID,totalHoursWork,totalSales);
+        System.out.println(fcs.toString());
+    }
+}
+
+class MyFCStaff{
+    private String name, staffID;
+    private double totalHoursWork, totalSales, totalSalary, commission;
+
+    public MyFCStaff(String name, String staffID,double totalHoursWork,double totalSales){
+        this.name = name;
+        this.staffID = staffID;
+        this.totalHoursWork = totalHoursWork;
+        this.totalSales = totalSales;
+    }
+
+    public double calculateCommission(){
+        if(totalSales>=150 && totalSales<=300){
+            commission = 0.05 * totalSales;
+        }else if(totalSales>=301 && totalSales<=500){
+            commission = 0.1 * totalSales;
+        }else if(totalSales>500){
+            commission = 0.15 * totalSales;
+        }else{
+            commission = 0;
+        }
+        return commission;
+    }
+
+    public double calculateSalary(){
+        totalSalary = totalHoursWork*8 + calculateCommission();
+        return totalSalary;
+    }
+
+    public String toString() {
+        String nm, id, hrs, sales, salary;
+        String myFormat1="%-15s%-2s%s\n";
+        String myFormat2="%-15s%-5s%.2f\n";
+        nm = String.format(myFormat1,"Staff Name",":",name);
+        id = String.format(myFormat1,"Staff ID",":",staffID);
+        hrs = String.format("%-15s%-2s%.0f\n","Hours Work",":",totalHoursWork);
+        sales = String.format(myFormat2,"Total Sale",": RM ",totalSales);
+        salary = String.format(myFormat2,"Total Salary",": RM ",calculateSalary());
+
+        return nm + id + hrs + sales + salary;
+    }
+}
